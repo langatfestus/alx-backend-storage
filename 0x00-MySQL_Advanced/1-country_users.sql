99c254e4edf6e9bd4019a051015655c72c318110
langatfestus <langatfestus94@gmail.com>
2023-12-14 20:34:08 +0300

    completed 0x00-MySQL_Advanced

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,15 @@
+-- SQL script that creates a table users following these requirements:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
+-- If the table already exists, your script should not fail
+-- Your script can be executed on any database
+
+
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
